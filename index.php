32590a3cf5a717953e886268568661b827d74a9b
nguyenash <44406859+nguyenash@users.noreply.github.com>
2019-11-13 19:41:38 -0500

    index
    Comma homepage!

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Author: "Ashley, Anna, Maimouna, Emma"
+ * Date: 11/13/2019
+ * File: index.php
+ * Description: Comma homepage
+ */ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<?php
+// add your php code here
+?>
+</body>
+</html>
